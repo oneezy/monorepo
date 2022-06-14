3c14a42b9965a033b6df4cf768c85e4199569f9f
Justin O'Neill <justinoneill2007@gmail.com>
2022-06-13 19:09:18 -0500

    breaking: breaking

--- apps/docs/src/routes/index.svelte
+++ apps/docs/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Buttton, Container } from '@oneezy/ui';
-  import Sweet from '$lib/Sweet.svelte';
-  // import { Accordion, AccordionItem } from "svelte-accessible-accordion";
+  import Sweet from '$root/apps/docs/src/lib/Sweet.svelte';
+  import { Accordion, AccordionItem } from "svelte-accessible-accordion";
 </script>
 
 <h1>Docs</h1>
@@ -11,8 +11,8 @@
 
 <Sweet />
 
-<!-- <Accordion class="accordion">
+<Accordion>
   <AccordionItem title="Title 1">Content 1</AccordionItem>
   <AccordionItem title="Title 2">Content 2</AccordionItem>
   <AccordionItem title="Title 3">Content 3</AccordionItem>
-</Accordion> -->
+</Accordion>
