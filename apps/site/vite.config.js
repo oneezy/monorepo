d5148ae00d559c439369d9611b911d9ac4deea0a
Justin O'Neill <justinoneill2007@gmail.com>
2022-08-23 17:42:07 -0500

    fix: vercel build attempt #6

--- apps/site/vite.config.js
+++ apps/site/vite.config.js
@@ -1,8 +1,35 @@
-import { viteConfig } from '@packages/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+
+const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  ...viteConfig
+	build: {
+		target: ['es2020'],
+	},
+	resolve: {
+		preserveSymlinks: false,
+		build: {
+			sourcemap: true
+		},
+	},
+	ssr: {
+		noExternal: Object.keys(pkg.dependencies || {})
+	},
+  server: {
+    open: '/'
+  },
+  plugins: [
+
+   /* Vitebook Fix: https://github.com/vitebook/vitebook/issues/89
+    *********************************************************************/
+    !process.env.VITEBOOK && sveltekit(),
+  ],
 };
+
+if (process.env.NODE_ENV === "production") config.resolve.preserveSymlinks = true;
 
 export default config;
