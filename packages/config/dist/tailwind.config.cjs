af4129d9b8b363f8be03c0029d5c9bd850e9646a
Justin ONeill <justinoneill2007@gmail.com>
2022-12-08 06:08:19 -0600

    fix: added back lib folder doh!

--- packages/config/dist/tailwind.config.cjs
+++ packages/config/dist/tailwind.config.cjs
@@ -4,11 +4,8 @@ const plugin = require('tailwindcss/plugin');
 
 const config = {
 	content: [
-		// '../../packages/components/**/*.{html,js,svelte,svx,ts}',
 		'../../packages/components/src/**/*.{html,js,svelte,md,svx,ts}',
 		'../../apps/*/src/**/*.{html,js,svelte,md,svx,ts}'
-		// './src/**/*.{html,js,svelte,md,svx,ts}',
-		// '../*/src/**/*.{html,js,svelte,md,svx,ts}'
 	],
 
 	darkMode: 'class',
